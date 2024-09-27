2bfe1ea2347bf092e48fa08a2a98a04329d74daf
Bruno Barreto <bruno-kk@hotmail.com>
2024-09-27 03:06:37 -0300

    fix:indices das migrações atualizadas

--- database/migrations/2024_09_25_174651_create_usuarios_table.php
+++ database/migrations/2024_09_25_174651_create_usuarios_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('usuarios', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary();
             $table->string('name')->nullable();
             $table->string('sobrenome')->nullable();
             $table->string('password')->nullable();
--- database/migrations/2024_09_25_182327_create_contratos_table.php
+++ database/migrations/2024_09_25_182327_create_contratos_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('contratos', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary();
             $table->string('numero_do_contrato');
             $table->string('nome_do_contratante');
             $table->string('data_de_inicio');
