b1eb181203f397217cbacac7369306415d7a1517
Bruno Barreto <Bruno-kk@hotmail.com>
2024-09-30 09:03:13 +0000

    fix:mudanças do servidor

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -28,6 +28,11 @@ class RouteServiceProvider extends ServiceProvider
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
 
+	Livewire::setUpdateRoute(function ($handle) {
+        $url =  'appname/public/livewire/update';
+        return Route::post($url, $handle);
+    });
+
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api')
